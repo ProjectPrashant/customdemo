073fe3178a33bec14ccccd68ebe684a7e2cafbb6
Pravin <pravin_j@customsoftdevelopers.com>
2017-09-01 16:15:17 +0530

    files add

---
+++ js/customfiltercontroller.js
@@ -0,0 +1,16 @@
+﻿var myApp= angular.module("myApplication",[])
+                   .controller("customfiltercontroller",function($scope){
+                     var listdata=[
+                {name:"Prashant",gender:1 ,salary:"20000",city:"Pune"},
+                         {name:"Madhav",gender:1 ,city:"Pune"},
+                         {name:"Sampat",gender:1,city:"Wifal"},
+                         {name:"Rakesh",gender:1,city:"Kolhapur"},
+                         {name:"Kirti",gender:2 ,city:"Nagpur"},
+                         {name:"Kajal",gender:2,city:"Mumbai"},
+                         {name:"Ujjany",gender:3,city:"Kanpur"}
+            ];
+                      
+                       $scope.employees=listdata;
+                     
+    });
+                     
